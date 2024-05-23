4a647c55b2fac089f0913e7d06f0fc7e6ac2fc66
Omadbek571 <omadbektomsk08@gmail.com>
2024-05-23 20:39:06 +0500

    .

---
+++ app.js
@@ -0,0 +1,150 @@
+// 1-savol
+
+// let KetishVaqt = +prompt("ILTIMOS KETISH VAQTINI KIRITING");
+// let KechikishVaqt = +prompt("ILTIMOS KECHIKISH VAQTINI KIRITING");
+// let natija = (KetishVaqt + KechikishVaqt) % 24;
+
+// document.write(natija);
+
+// 2-savol
+
+// let celsius = 25;
+// let kelvin = celsius + 273.15;
+// let fahrenheit = celsius * 1.80 + 32.00;
+
+// document.write(`${celsius}°C = ${kelvin}K = ${fahrenheit}°F`);
+
+
+// 3-savol
+
+// let N = +prompt("Validan N yoshga kattaligini aytdi");
+// let K = +prompt("Vali Alidan K marta kichikligini aytdi");
+
+// let bilmasvoyYoshi = N
+// let valijonYoshi = N + 1
+
+// let alijoniYoshi = bilmasvoyYoshi - 1
+
+
+// document.write("ALIJONI YOSHI   " + alijoniYoshi);
+// document.write("valijon YOSHI   " + valijonYoshi);
+// document.write("bilmasvoy YOSHI   " + bilmasvoyYoshi);
+
+// 4-savol
+
+// let kg = +prompt("ILTIMOS TARVUZ KG KIRITING");
+// let BirinchiFoyiz = +prompt("NECHI FOYIZI IBORAT SUVDAN YANGILIGIDA");
+// let IkkinchiFoyiz = +prompt("NECHI FOYIZI IBORAT SUVDAN KEGIN");
+
+// let BirinchiQoldiq = kg * (100 - BirinchiFoyiz) / 100;
+// let IkkinchiQoldiq = kg * (100 - IkkinchiFoyiz) / 100
+
+// let natija = kg - (BirinchiQoldiq + IkkinchiQoldiq)
+
+// document.write(natija);
+
+
+// 5-savol
+
+
+// let son = +prompt("ILTIMOS QIYMAT KIRITING")
+
+// let natija = (son - 2) * 180;
+
+// document.write("ICHKI BURCHAK YIGINDISI  " + natija);
+
+
+// 6-savol
+
+// let soat = +prompt("HOZIRGI SOATI KIRITING");
+// let minut = +prompt("HOZIRGI MINUTI KITING");
+// let voqt = 45;
+
+// let keyingiSoat = (soat + Math.floor((minut + voqt) / 60)) % 24;
+// let keyingiMinut = (minut + voqt) % 60;
+
+// if (keyingiMinut < 10) {
+//     keyingiMinut = "0" + keyingiMinut;
+// }
+
+// document.write("Keyingi vaqt:", keyingiSoat + ":" + keyingiMinut);
+
+
+// 7-savol
+
+// let BuvisiniYoshi = +prompt("Iltimos sardordi buvasini yoshini kiriting");
+// let SardordiYoshi = +prompt("Iltimos sardordi yoshini kiriting");
+
+// let natija = (BuvisiniYoshi - SardordiYoshi) / 2
+
+// document.write("SARDOR SHUNCHA  " + natija + "    YOSHDAN SONG BUVASI YARIM YOSHIGA ETADI EKAN");
+
+
+// 8-savol
+
+
+// let DirektorOyligi = +prompt("Iltimos Direktor oyligini kiriting")
+// let Tester = +prompt("Iltimos tester oyligini kiring")
+// let natija = (DirektorOyligi + Tester) / 2
+
+// document.write("Ortacha oylik  " + natija)
+
+
+// 9-savol
+
+// let son = +prompt("Iltimos son kiriting");
+
+// let natija = (son * (son - 3)) / 2
+
+// document.write(natija);
+
+
+// 10-savol
+
+// let A = +prompt("Iltimos ko'rmoqchi bo'lgan kinoingizning davomiyligini minutlarda kiriting"); // Video davomiyligi
+// let B = +prompt("Pauza vaqtini kiriting sekundlarda"); // Pauza davomiyligi
+
+// let minutiSecundgaOtkazgich = A * 60; 
+// let natija = minutiSecundgaOtkazgich - B; 
+// let natijaMin = natija / 60; 
+
+// document.write(natija + " sekunda bo'ladi ekan<br>");
+// document.write(Math.floor(natijaMin) + " minuta bo'ladi ekan");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
